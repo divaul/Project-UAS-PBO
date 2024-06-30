3d3f12131468d61f571eddbed2c20fd40bc7b40c
divaul <150714066+divaul@users.noreply.github.com>
2024-06-30 21:51:02 +0700

    Update Item.java

--- src/tokobukupbo/Item.java
+++ src/tokobukupbo/Item.java
@@ -11,18 +11,21 @@ package tokobukupbo;
 public class Item {
     private String judul;
     private String penerbit;
+    private String namaPengarang;
     private int tahunCetak;
     private String kategori;
 
     public Item(String judul, String penerbit, int tahunCetak, String kategori) {
         this.judul = judul;
         this.penerbit = penerbit;
+        this.namaPengarang = namaPengarang;
         this.tahunCetak = tahunCetak;
         this.kategori = kategori;
     }
 
     public String getJudul() { return judul; }
     public String getPenerbit() { return penerbit; }
+    public String getNamaPengarang() { return namaPengarang; }
     public int getTahunCetak() { return tahunCetak; }
     public String getKategori() { return kategori; }
 
@@ -30,6 +33,7 @@ public class Item {
     public String toString() {
         return "Judul: " + judul + "\n" +
                "Penerbit: " + penerbit + "\n" +
+               "Nama Pengarang: " + namaPengarang +
                "Tahun Cetak: " + tahunCetak + "\n" +
                "Kategori: " + kategori;
     }
